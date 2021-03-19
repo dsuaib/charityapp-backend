539f8dfd2005d534bbfe8a24fbc491050faa1f1f
dsuaib <71049472+dsuaib@users.noreply.github.com>
2021-03-19 06:00:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const routesUrls = require('./routes')
+const cors = require('cors')
+
+dotenv.config()
+
+
+app.use(express.json())
+app.use(cors())
+app.use('/app', routesUrls)
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+const port = process.env.PORT||5000;
+mongoose.connect(process.env.DATABASECONNECTION)
+.then(() => app.listen(port, () => console.log (`Database connected and server is running!: ${port}`)))
+    
