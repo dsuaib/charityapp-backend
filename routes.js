57c1a8284b10d7123c7dd8fc374e867d032f9917
dsuaib <71049472+dsuaib@users.noreply.github.com>
2021-03-20 21:04:26 -0400

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+const registerMemberAccountCopy = require('./registerMemberModel')
+const GetRegisteredMember = require('./registerMemberModel')
+
+router.post('/registermember', (request, response) => {
+    const registeredMember = new registerMemberAccountCopy({
+        firstName:request.body.firstName,
+        lastName:request.body.lastName,
+        username:request.body.username,
+        password:request.body.password,
+        age:request.body.age,
+        donations:request.body.donations
+    })
+     registeredMember.save()
+    .then(data=>{
+        response.json(data)
+    })
+    .catch(error=>{
+        response.json(error)
+    })
+})
+
+router.get('/member', (req, res) => {
+    res.send('member')
+})
+
+router.get('/members', async (req, res) => {
+    const member = await GetRegisteredMember.find()
+    res.status(200).json(member)
+})
+
+router.get('/members/:id', async (req, res) => {
+    var id = req.params.id
+    const getmember = await GetRegisteredMember.findById(id)
+    res.status(200).json( getmember)
+    console.log(getmember)
+})
+
+
+router.post('/createannouncement', async (request, response) => {
+    const createdAnnoucement = new createAnnouncementCopy({
+        fullName:request.body.fullName,
+        title:request.body.title,
+        message:request.body.message,
+    })
+    await createdAnnoucement.save()
+    .then(data=>{
+        response.json(data)
+    })
+    .catch(error=>{
+        response.json(error)
+    })
+})
+
+module.exports = router
